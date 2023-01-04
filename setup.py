062a547d72dc55be2384e6a770fcb00f390bc548
mrMekdad <mekdadr6@gmail.com>
2023-01-04 01:20:00 +0100

    atacseq-peaks-workbench: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2023-atacseq-peaks-workbench",
+    version="0.1.0",
+    description="Workbench for peak-set bookkeeping, annotations, and comparative exports.",
+    author="Red@",
+    packages=["atacseq_peaks_workbench"],
+    package_dir={"": "src"},
+)
